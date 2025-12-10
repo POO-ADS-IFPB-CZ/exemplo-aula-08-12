661ab7b03a0991f0675b58bbdbc22c43355b6ebe
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-12-10 08:25:28 -0300

    Exemplo de showMessage

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,39 +1,16 @@
 package view;
 
-import exception.EmailDuplicadoException;
-import exception.NomeInvalidoException;
-import exception.SenhaInvalidaException;
-import model.Usuario;
-import service.UsuarioService;
-
 import javax.swing.*;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        UsuarioService usuarioService = null;
-        try {
-            usuarioService = new UsuarioService();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
-        try {
-            usuarioService.create(new Usuario("joao@gmail.com",
-                    "João", "123456"));
-        } catch (SenhaInvalidaException e) {
-            System.out.println(e.getMessage());
-        } catch (EmailDuplicadoException e) {
-
-        } catch (IOException e) {
-
-        } catch (ClassNotFoundException e) {
-
-        } catch (NomeInvalidoException e) {
-
-        }
+        ImageIcon icon = new ImageIcon("imgs/error.png");
+        JOptionPane.showMessageDialog(null,
+                "Hello World", "Mudando o título",
+                JOptionPane.ERROR_MESSAGE,
+                icon);
 
     }
 
