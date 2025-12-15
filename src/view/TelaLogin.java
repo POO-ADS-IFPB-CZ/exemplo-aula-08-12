ef2cb9a739c0c3cb0bcec0b94bc6b3fce61bb611
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-12-15 08:02:37 -0300

    Criando a tela

---
+++ src/view/TelaLogin.java
@@ -0,0 +1,34 @@
+package view;
+
+import javax.swing.*;
+
+public class TelaLogin extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+
+    public TelaLogin() {
+        setContentPane(contentPane);
+        setModal(true);
+        setTitle("Tela de Login");
+        //Sempre no topo
+        setAlwaysOnTop(true);
+        //Alterando o tamanho da tela
+        setSize(600,600);
+        //Abrir no centro da tela
+        setLocationRelativeTo(null);
+        //Alterando o ícone da tela
+        ImageIcon icon = new ImageIcon("src/imgs/cadeado.png");
+        setIconImage(icon.getImage());
+        getRootPane().setDefaultButton(buttonOK);
+    }
+
+    public static void main(String[] args) {
+        TelaLogin dialog = new TelaLogin();
+//        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
