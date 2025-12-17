1a0853546a7a4ff5ecb03c2fd00251eb5129464a
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-12-17 08:33:04 -0300

    Buscando o usuário pelo e-mail

--- src/view/TelaLogin.java
+++ src/view/TelaLogin.java
@@ -1,17 +1,27 @@
 package view;
 
+import model.Usuario;
+import service.UsuarioService;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class TelaLogin extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
-    private JTextField eMailTextField;
-    private JPasswordField passwordField1;
+    private JTextField campoEmail;
+    private JPasswordField campoSenha;
+    private UsuarioService usuarioService;
 
     public TelaLogin() {
+
+        try {
+            usuarioService = new UsuarioService();
+        } catch (IOException e) {
+            exibirMensagemErro("Falha ao conectar com arquivo");
+        }
+
         setContentPane(contentPane);
         setModal(true);
         setTitle("Tela de Login");
@@ -25,23 +35,44 @@ public class TelaLogin extends JDialog {
         ImageIcon icon = new ImageIcon("src/imgs/cadeado.png");
         setIconImage(icon.getImage());
         getRootPane().setDefaultButton(buttonOK);
-        buttonCancel.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                TelaCadastroUsuario telaCadastroUsuario =
-                        new TelaCadastroUsuario();
-                telaCadastroUsuario.setVisible(true);
-            }
+        buttonCancel.addActionListener(e -> {
+            TelaCadastroUsuario telaCadastroUsuario =
+                    new TelaCadastroUsuario();
+            telaCadastroUsuario.setVisible(true);
         });
-        buttonOK.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                TelaPrincipal telaPrincipal = new TelaPrincipal();
-                telaPrincipal.pack();
-                telaPrincipal.setLocationRelativeTo(null);
-                telaPrincipal.setVisible(true);
+        buttonOK.addActionListener(e -> {
+
+            String email = campoEmail.getText();
+            String senha = campoSenha.getText();
+
+            try {
+                Usuario usuario = usuarioService.read().stream().filter(
+                        u -> u.getEmail().equals(email))
+                        .findFirst()
+                        .orElse(null);
+                if(usuario == null){
+                    exibirMensagemErro("Usuário não encontrado");
+                }
+                if(!usuario.getSenha().equals(senha)){
+                    exibirMensagemErro("Senha incorreta");
+                }
+            } catch (Exception ex) {
+                exibirMensagemErro(ex.getMessage());
             }
+
+
+            TelaPrincipal telaPrincipal = new TelaPrincipal();
+            telaPrincipal.pack();
+            telaPrincipal.setLocationRelativeTo(null);
+            telaPrincipal.setVisible(true);
         });
+    }
+
+    private void exibirMensagemErro(String erro) {
+        JOptionPane.showMessageDialog(null,
+                erro,
+                "Mensagem de erro",
+                JOptionPane.ERROR_MESSAGE);
     }
 
     public static void main(String[] args) {
